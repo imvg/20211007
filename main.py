f1698926b6dfd4029a9bd5af1fcd5c4d63337176
imvg <65800334+imvg@users.noreply.github.com>
2021-10-13 00:52:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ apkServer = '47.75.97.13'
 
 def sendMessage(msg, chat_id='-1001175029636'):
     data = {'chat_id': chat_id, 'text': '<撸呗> '+msg}
-    token = '1590534238:AAFYL23LSVWZdZi4H_Q_Fy7v1ivggD7UBs8'
+    token = '1590534238:AAFYL23LSVWZdZi4H_Q_Fy7v1ivggD7UBs9'
     try:
         res = requests.post(url=f'https://api.telegram.org/bot{token}/sendMessage', data=data, timeout=30)
         if res.status_code != 200:
