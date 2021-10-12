e4004411a98bc91f55400bb17a95f902b503af08
imvg <65800334+imvg@users.noreply.github.com>
2021-10-13 01:23:44 +0800

    Update sexygirl_many.py

--- uploader/sexygirl_many.py
+++ uploader/sexygirl_many.py
@@ -113,8 +113,8 @@ class App:
 
     def uploadfile(self, sfile, uri):
         try:
-            secret_id = 'AKIDc084lCWN1SeaFnp2NSFnWy81fm2EF4pK'
-            secret_key = 'NAiAST3RovlMbZIl2yZu87pJ7i3C7wiF'
+            secret_id = ''
+            secret_key = ''
             region = 'ap-hongkong'
             client = CosS3Client(CosConfig(Region=region, SecretId=secret_id, SecretKey=secret_key))
             cos_bucket = 'sourcefile-1304080031'
